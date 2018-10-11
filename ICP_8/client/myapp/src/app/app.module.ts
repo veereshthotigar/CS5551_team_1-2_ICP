7931f9bbfd884bc5266d6a6a46970e394fcd0a09
veereshthotigar <veeresh.thotygar@gmail.com>
2018-10-11 17:21:02 -0500

    parsing the data

--- ICP_8/client/myapp/src/app/app.module.ts
+++ ICP_8/client/myapp/src/app/app.module.ts
@@ -1,9 +1,10 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { HttpClientModule } from '@angular/common/http';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './pages/home/home.component';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
@@ -12,6 +13,8 @@ import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
   ],
   imports: [
     BrowserModule,
+    HttpClientModule,
+    FormsModule,
     NgbModule.forRoot()
   ],
   providers: [],
--- ICP_8/client/myapp/src/app/pages/home/home.component.ts
+++ ICP_8/client/myapp/src/app/pages/home/home.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-home',
@@ -8,8 +10,28 @@ import { Component, OnInit } from '@angular/core';
 export class HomeComponent implements OnInit {
   //https://rxnav.nlm.nih.gov/REST/interaction/interaction.json?rxcui=88014&sources=ONCHigh
   //https://rxnav.nlm.nih.gov/REST/interaction/interaction.json?rxcui=384455&sources=DrugBank
-  constructor() { }
-
+  dataSources:string[];
+  rxcui:string;
+  dataSourceSelected:string = 'DrugBank';
+  url:string;
+  result:Observable<any>;
+  constructor(private http: HttpClient) {
+    this.rxcui = "88014";
+    this.dataSources = ['ONCHigh','DrugBank'];
+   }
+  selectDataSource(src:string){
+    this.dataSourceSelected = src;
+  }
+  retriveData(){
+    this.url = 'https://rxnav.nlm.nih.gov/REST/interaction/interaction.json?rxcui='+this.rxcui+'&sources='+this.dataSourceSelected;
+    this.http.get(this.url)
+      .subscribe(
+        (res:any)=>{
+          this.result = res.interactionTypeGroup[0].interactionType;
+          console.log(this.result);
+        }
+      )
+  }
   ngOnInit() {
   }
 
